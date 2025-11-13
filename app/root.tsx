bfb91363830317cccf41f515994b60e6a812f8fe
J0rd1c3 <jordi@ticketdoor.com>
2025-11-13 22:31:29 +0100

    first commit

--- app/root.tsx
+++ app/root.tsx
@@ -5,10 +5,10 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "react-router";
+} from "react-router"
 
-import type { Route } from "./+types/root";
-import "./app.css";
+import type { Route } from "./+types/root"
+import "./app.css"
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -21,7 +21,11 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-];
+  {
+    rel: "icon",
+    href: "https://emojicdn.elk.sh/🎱",
+  },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -38,38 +42,38 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
+  let message = "Oops!"
+  let details = "An unexpected error occurred."
+  let stack: string | undefined
 
   if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
+    message = error.status === 404 ? "404" : "Error"
     details =
       error.status === 404
         ? "The requested page could not be found."
-        : error.statusText || details;
+        : error.statusText || details
   } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
+    details = error.message
+    stack = error.stack
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
+    <main className="container mx-auto p-4 pt-16">
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+        <pre className="w-full overflow-x-auto p-4">
           <code>{stack}</code>
         </pre>
       )}
     </main>
-  );
+  )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,92 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Work Journal" },
     { name: "description", content: "Welcome to React Router!" },
-  ];
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="m-auto mt-20 flex max-w-sm flex-col">
+      <div className="w-full">
+        <h1 className="text-3xl font-bold">Work Journal</h1>
+        <p className="text-gray-500">Learning and doing. Updated weekly</p>
+      </div>
+      <div className="mt-10 w-full rounded-md border-2 border-dashed border-gray-200 p-2">
+        <form action="">
+          <p className="font-medium italic">Create an entry</p>
+          {/* date */}
+          <div className="mt-2 w-fit">
+            <input className="rounded-md" type="date" name="date" id="date" />
+          </div>
+          {/* category */}
+          <div className="mt-2 flex gap-3">
+            <label
+              className="flex items-center hover:cursor-pointer"
+              htmlFor="work"
+            >
+              <input
+                type="radio"
+                className="mr-2"
+                name="category"
+                value="work"
+                id="work"
+              />
+              Work
+            </label>
+            <label
+              className="flex items-center hover:cursor-pointer"
+              htmlFor="learning"
+            >
+              <input
+                type="radio"
+                className="mr-2"
+                name="category"
+                value="learning"
+                id="learning"
+              />
+              Learning
+            </label>
+            <label
+              className="flex items-center hover:cursor-pointer"
+              htmlFor="interesting-thing"
+            >
+              <input
+                type="radio"
+                className="mr-2"
+                name="category"
+                value="interesting-thing"
+                id="interesting-thing"
+              />
+              Interesting
+            </label>
+          </div>
+          {/* content */}
+          <div className="mt-2">
+            <textarea
+              className="w-full rounded-md border-gray-300"
+              name="content"
+              id="content"
+              rows={4}
+              placeholder="Write your journal entry here..."
+            ></textarea>
+          </div>
+          {/* submit button */}
+          <div className="mt-2 w-full text-right">
+            <button
+              type="submit"
+              className="rounded-md bg-blue-600 px-4 py-2 font-medium text-white hover:bg-blue-700"
+            >
+              Add Entry
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="mt-10">
+        <p>No entries! Write your first journal entry.</p>
+      </div>
+    </main>
+  )
 }
