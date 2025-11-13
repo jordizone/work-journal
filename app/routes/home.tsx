3e45dd8a9eccb83ea8409f4626607684c492bf9a
J0rd1c3 <jordi@ticketdoor.com>
2025-11-13 23:44:47 +0100

    feat: add Prisma setup with SQLite datasource and initial schema
    - Updated package.json to include Prisma dependencies and dotenv for environment variable management.
    - Created prisma.config.ts for Prisma configuration with environment variable support.
    - Added a new SQLite database file (dev.db) for local development.
    - Introduced schema.prisma defining the Entry model with fields for id, date, category, and content.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,39 @@
+import { Form, redirect } from "react-router"
 import type { Route } from "./+types/home"
+import { PrismaClient } from "generated/prisma/client"
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Work Journal" },
     { name: "description", content: "Welcome to React Router!" },
   ]
+}
+
+export async function action({ request }: Route.ActionArgs) {
+  let db = new PrismaClient()
+  let formData = await request.formData()
+  let data = Object.fromEntries(formData)
+  console.log("Form data:", data)
+  let { date, category, content } = data
+
+  if (
+    typeof date !== "string" ||
+    typeof category !== "string" ||
+    typeof content !== "string"
+  ) {
+    throw new Error("Invalid form data")
+  }
+
+  // Here you would typically save the data to a database
+  await db.entry.create({
+    data: {
+      date: new Date(date),
+      category,
+      content,
+    },
+  })
+
+  return redirect("/")
 }
 
 export default function Home() {
@@ -15,7 +44,7 @@ export default function Home() {
         <p className="text-gray-500">Learning and doing. Updated weekly</p>
       </div>
       <div className="mt-10 w-full rounded-md border-2 border-dashed border-gray-200 p-2">
-        <form action="">
+        <Form method="post">
           <p className="font-medium italic">Create an entry</p>
           {/* date */}
           <div className="mt-2 w-fit">
@@ -66,7 +95,7 @@ export default function Home() {
           {/* content */}
           <div className="mt-2">
             <textarea
-              className="w-full rounded-md border-gray-300"
+              className="w-full rounded-md border-gray-300 focus-within:border-blue-500 focus-within:ring-1 focus-within:ring-blue-500"
               name="content"
               id="content"
               rows={4}
@@ -77,12 +106,12 @@ export default function Home() {
           <div className="mt-2 w-full text-right">
             <button
               type="submit"
-              className="rounded-md bg-blue-600 px-4 py-2 font-medium text-white hover:bg-blue-700"
+              className="rounded-md bg-blue-600 px-4 py-2 font-medium text-white transition-colors focus-within:ring-2 focus-within:ring-blue-700 focus-within:ring-offset-2 focus-within:outline-none hover:cursor-pointer hover:bg-blue-400"
             >
               Add Entry
             </button>
           </div>
-        </form>
+        </Form>
       </div>
       <div className="mt-10">
         <p>No entries! Write your first journal entry.</p>
